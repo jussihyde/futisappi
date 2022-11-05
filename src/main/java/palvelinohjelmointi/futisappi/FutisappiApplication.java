55b4ffe5cbfc0d0734d27a58aac4056e09362935
Jussi <Jussi@MacBook-Pro-2.local>
2022-11-05 18:11:59 +0200

    first commit

---
+++ src/main/java/palvelinohjelmointi/futisappi/FutisappiApplication.java
@@ -0,0 +1,13 @@
+package palvelinohjelmointi.futisappi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutisappiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutisappiApplication.class, args);
+	}
+
+}
